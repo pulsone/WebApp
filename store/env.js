8d7f2d2f97cb8e35b77f35dfc000d552855b47fb
Robert Schäfer <git@roschaefer.de>
2018-08-03 22:58:00 +0200

    Refactor all store modules to use classic mode

--- store/env.js
+++ store/env.js
@@ -1,25 +1,24 @@
-export const state = () => {
-  return {
+export default {
+  state: {
     env: {}
-  }
-}
+  },
+  mutations: {
+    SET_ENV (state, env) {
+      state.env = env
+    }
+  },
 
-export const mutations = {
-  SET_ENV (state, env) {
-    state.env = env
-  }
-}
+  getters: {
+    all (state) {
+      return state.env
+    }
+  },
 
-export const getters = {
-  all (state) {
-    return state.env
-  }
-}
-
-export const actions = {
-  init ({commit}) {
-    if (process.server) {
-      commit('SET_ENV', require('dotenv').config().parsed)
+  actions: {
+    init ({commit}) {
+      if (process.server) {
+        commit('SET_ENV', require('dotenv').config().parsed)
+      }
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,17 +1,37 @@
 import feathersVuex, { initAuth } from 'feathers-vuex'
 import feathersClient from '../feathers-client'
+import Vuex from 'vuex'
+
+import auth from './auth';
+import categories from './categories';
+import comments from './comments';
+import connection from './connections';
+import env from './env';
+import layout from './layout';
+import newsfeed from './newsfeed';
+import notifications from './notifications';
+import organizations from './organizations';
+import search from './search';
+import settings from './settings';
+import usersettings from './usersettings';
 
 const { service } = feathersVuex(feathersClient, { idField: '_id' })
 
-
-export const actions = {
-  async nuxtServerInit ({dispatch}) {
-    dispatch('categories/init')
-    await dispatch('auth/init')
-    await dispatch('settings/init')
-  }
+const createStore = () => {
+  return new Vuex.Store({
+    modules: { auth, categories, comments, connection, env, layout, newsfeed, notifications, organizations, search, settings, usersettings },
+    actions: {
+      async nuxtServerInit ({dispatch}) {
+        dispatch('categories/init')
+        await dispatch('auth/init')
+        await dispatch('settings/init')
+      }
+    },
+    plugins: [
+      service('usersettings')
+    ]
+  });
 }
 
-export const plugins = [
-  service('usersettings')
-]
+export default createStore
+
